0758e8173d06b1bda9a1c46f48d59e4c16034306
ElRecipiente <nicolas.droux.2b@gmail.com>
2023-02-23 15:58:20 +0100

    show more not fixed yet

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-let theMain = document.querySelector("main");
+let theMainDiv = document.querySelector("main > div");
+let theLastDiv = document.querySelector("main > div:last-child")
 let currentLanguage;
 let currentCategory;
 let selectLanguage = document.querySelector("select#language");
 let selectCategory = document.querySelector("select#category");
 let theImg = document.querySelector("nav a img")
-let showMoreOrLessButton = document.querySelector("footer button");
+let showMoreOrLessButton = document.getElementById("showButton");
 let data;
 let showMore = false;
+let whatIsShow = 0;
 let maxLength;
+let arrowButtons = document.querySelectorAll("main button");
 
 async function updateContent(url) {
     let response = await fetch(url);
@@ -18,45 +21,127 @@ async function updateContent(url) {
 
 function showArticles() {
 
-    if (!showMore) {
+    if (whatIsShow == 0) {
         maxLength = 3;
         for (let i = 0; i < maxLength; i++) {
 
             if (data.articles[i].urlToImage == null) {
                 maxLength = maxLength + 1;
-                console.log("brouette")
             }
 
             else if (data.articles[i].urlToImage != null) {
                 let newArticle = document.createElement("article")
                 newArticle.innerHTML = `<h2>${data.articles[i].title}</h2>
-        <a href="${data.articles[i].url}"><img src="${data.articles[i].urlToImage}" alt="img/404.jpg"></a>
-        <div>${data.articles[i].description}</div>
-        <p>${(data.articles[i].author == null) ? "Un illustre inconnu" : data.articles[i].author}</p>`
-                setTimeout(() => {
-                    newArticle.style.opacity = "1"
-                }, (i * 200));
-                theMain.append(newArticle)
-            }
-        }
-    }
-
-    else {
-
-        for (let i = maxLength; i < data.articles.length; i++) {
-
-            if (data.articles[i].urlToImage != null) {
-                let newArticle = document.createElement("article")
-                newArticle.innerHTML += `<h2>${data.articles[i].title}</h2>
         <a href="${data.articles[i].url}"><img src="${data.articles[i].urlToImage}" alt="Image Indisponible"></a>
         <div>${data.articles[i].description}</div>
         <p>${(data.articles[i].author == null) ? "Un illustre inconnu" : data.articles[i].author}</p>`
                 setTimeout(() => {
                     newArticle.style.opacity = "1"
-                }, (i * 200));
-                theMain.append(newArticle)
+                }, (200));
+                theMainDiv.append(newArticle)
             }
         }
+    }
+
+    else if (whatIsShow == 1) {
+
+        for (let i = maxLength; i < maxLength + 3; i++) {
+
+            if (data.articles[i].urlToImage == null) {
+                maxLength = maxLength + 1;
+            }
+
+            else if (data.articles[i].urlToImage != null) {
+                let newArticle = document.createElement("article")
+                newArticle.innerHTML = `<h2>${data.articles[i].title}</h2>
+        <a href="${data.articles[i].url}"><img src="${data.articles[i].urlToImage}" alt="Image Indisponible"></a>
+        <div>${data.articles[i].description}</div>
+        <p>${(data.articles[i].author == null) ? "Un illustre inconnu" : data.articles[i].author}</p>`
+                setTimeout(() => {
+                    newArticle.style.opacity = "1"
+                }, (200));
+                theMainDiv.append(newArticle)
+            }
+        }
+    }
+
+    else if (whatIsShow == 2) {
+
+        for (let i = maxLength + 3; i < maxLength + 6; i++) {
+
+            if (data.articles[i].urlToImage == null) {
+                maxLength = maxLength + 1;
+            }
+
+            else if (data.articles[i].urlToImage != null) {
+                let newArticle = document.createElement("article")
+                newArticle.innerHTML = `<h2>${data.articles[i].title}</h2>
+        <a href="${data.articles[i].url}"><img src="${data.articles[i].urlToImage}" alt="Image Indisponible"></a>
+        <div>${data.articles[i].description}</div>
+        <p>${(data.articles[i].author == null) ? "Un illustre inconnu" : data.articles[i].author}</p>`
+                setTimeout(() => {
+                    newArticle.style.opacity = "1"
+                }, (200));
+                theMainDiv.append(newArticle)
+            }
+        }
+    }
+
+    else if (whatIsShow == 3) {
+
+        for (let i = maxLength + 6; i < maxLength + 9; i++) {
+
+            if (data.articles[i].urlToImage == null) {
+                maxLength = maxLength + 1;
+            }
+
+            else if (data.articles[i].urlToImage != null) {
+                let newArticle = document.createElement("article")
+                newArticle.innerHTML = `<h2>${data.articles[i].title}</h2>
+        <a href="${data.articles[i].url}"><img src="${data.articles[i].urlToImage}" alt="Image Indisponible"></a>
+        <div>${data.articles[i].description}</div>
+        <p>${(data.articles[i].author == null) ? "Un illustre inconnu" : data.articles[i].author}</p>`
+                setTimeout(() => {
+                    newArticle.style.opacity = "1"
+                }, (200));
+                theMainDiv.append(newArticle)
+            }
+        }
+    }
+
+
+    if (showMore) {
+        for (let i = 0; i < data.articles.length; i++) {
+
+            if (data.articles[i].urlToImage != null) {
+
+                let newArticle = document.createElement("article")
+                newArticle.innerHTML += `<h2>${data.articles[i].title}</h2>
+        <a href="${data.articles[i].url}"><img src="${data.articles[i].urlToImage}" alt="Image Indisponible"></a>`
+                setTimeout(() => {
+                    newArticle.style.opacity = "1"
+                }, (i * 200));
+                theLastDiv.append(newArticle)
+            }
+        }
+    }
+}
+
+function pressRightButton() {
+    if (whatIsShow < 3) {
+        whatIsShow += 1;
+        theMainDiv.innerHTML = "";
+        showArticles();
+    }
+
+
+}
+
+function pressLeftButton() {
+    if (whatIsShow > 0) {
+        whatIsShow -= 1;
+        theMainDiv.innerHTML = "";
+        showArticles();
     }
 }
 
@@ -64,14 +149,17 @@ function showMoreOrLess() {
 
     if (!showMore) {
         showMore = true;
+        theLastDiv.innerHTML = "";
+        theMainDiv.innerHTML = "";
         showArticles();
         showMoreOrLessButton.textContent = "Show Less";
     }
     else {
         showMore = false;
-        theMain.innerHTML = "";
+        theLastDiv.innerHTML = "";
+        theMainDiv.innerHTML = "";
         showArticles();
-        showMoreOrLessButton.textContent = "Show More";
+        showMoreOrLessButton.textContent = "Show All";
     }
 }
 
@@ -87,10 +175,10 @@ async function myGifUpdate(url) {
 selectCategory.addEventListener("change", () => {
     currentLanguage = selectLanguage.value;
     currentCategory = selectCategory.value;
-    theMain.innerHTML = "";
+    theMainDiv.innerHTML = "";
     showMore = false;
-    showMoreOrLessButton.textContent = "Show More";
-    updateContent(`https://newsapi.org/v2/top-headlines?category=${currentCategory}&country=${currentLanguage}&sortBy=publishedAt&apiKey=c80cfc2928404db29d71bf9af9cdc196`);
+    showMoreOrLessButton.textContent = "Show All";
+    updateContent(`https://newsapi.org/v2/top-headlines?category=${currentCategory}&country=${currentLanguage}&sortBy=publishedAt&apiKey=a5248deeccb74779bd3657617913a0ad`);
 })
 
 
@@ -98,10 +186,10 @@ selectCategory.addEventListener("change", () => {
 selectLanguage.addEventListener("change", () => {
     currentLanguage = selectLanguage.value;
     currentCategory = selectCategory.value;
-    theMain.innerHTML = "";
+    theMainDiv.innerHTML = "";
     showMore = false;
-    showMoreOrLessButton.textContent = "Show More";
-    updateContent(`https://newsapi.org/v2/top-headlines?category=${currentCategory}&country=${currentLanguage}&sortBy=publishedAt&apiKey=c80cfc2928404db29d71bf9af9cdc196`);
+    showMoreOrLessButton.textContent = "Show All";
+    updateContent(`https://newsapi.org/v2/top-headlines?category=${currentCategory}&country=${currentLanguage}&sortBy=publishedAt&apiKey=a5248deeccb74779bd3657617913a0ad`);
 })
 
 const debounce = (func, wait) => {
@@ -126,9 +214,9 @@ searchInput.addEventListener("input", debounce(() => {
     let filter = searchInput.value;
     currentLanguage = selectLanguage.value;
     currentCategory = selectCategory.value;
-    theMain.innerHTML = "";
-    updateContent(`https://newsapi.org/v2/top-headlines?category=${currentCategory}&country=${currentLanguage}&sortBy=publishedAt&apiKey=c80cfc2928404db29d71bf9af9cdc196&q=${filter}`);
+    theMainDiv.innerHTML = "";
+    updateContent(`https://newsapi.org/v2/top-headlines?category=${currentCategory}&country=${currentLanguage}&sortBy=publishedAt&apiKey=a5248deeccb74779bd3657617913a0ad&q=${filter}`);
 }, 1000))
 
-updateContent("https://newsapi.org/v2/top-headlines?country=fr&sortBy=publishedAt&apiKey=c80cfc2928404db29d71bf9af9cdc196");
+updateContent("https://newsapi.org/v2/top-headlines?country=fr&sortBy=publishedAt&apiKey=a5248deeccb74779bd3657617913a0ad");
 myGifUpdate("https://api.giphy.com/v1/gifs/search?api_key=1cK5IjzDd0Q0edYKVZZMwhCONAyfoZq9&q=newspaper&limit=10&offset=0&rating=g&lang=en");
